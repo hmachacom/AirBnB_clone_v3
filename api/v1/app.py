ea8ffa5d2a94af3940a19251a4ffb8c6b71e1ea8
Hugo Machacon <hmachacom@gmail.com>
2022-05-30 11:29:11 -0500

    @app.errorhandler(404) in api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from models import storage
 from os import getenv
 from flask import Flask
 from api.v1.views import app_views
+from flask import jsonify, make_response
 
 
 app = Flask(__name__)
@@ -17,6 +18,12 @@ app.register_blueprint(app_views)
 def close_storage_new(self):
     """request you must remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """error 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
